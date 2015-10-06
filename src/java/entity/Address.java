fd966f3fd2d6646388e4d186eaaf43acd20092e0
ruvazi <Runevazi@gmail.com>
2015-10-06 11:00:16 +0200

    CA2 -Entity

--- src/java/entity/Address.java
+++ src/java/entity/Address.java
@@ -6,14 +6,49 @@
 package entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  *
  * @author Rune
  */
 @Entity
-public class Address extends InfoCity{
+public class Address{
+    @Id
+    private Long id;
     private String Address;
     private String AdditionalInfo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address() {
+    }
+
+    public Address(String Address, String AdditionalInfo) {
+        this.Address = Address;
+        this.AdditionalInfo = AdditionalInfo;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public String getAdditionalInfo() {
+        return AdditionalInfo;
+    }
+
+    public void setAdditionalInfo(String AdditionalInfo) {
+        this.AdditionalInfo = AdditionalInfo;
+    }
     
 }
